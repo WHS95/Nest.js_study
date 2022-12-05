d6eb876dd8187786c0f505973a07a7802a5b1297
swh_95 <tjdngur22@gmail.com>
2022-12-05 13:08:47 +0900

    ADD(logger.middleware: 미들웨어를 통한 logging)

--- src/logger.middleware.ts
+++ src/logger.middleware.ts
@@ -1,11 +1,19 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
+//nestjs의 logger클래스를 이용하여 logging 진행
+//res응답할때 까지 의 값도 같이 logging 할 경우
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.ip);
-    console.log(req.originalUrl);
+    res.on('finish', () => {
+      this.logger.log(
+        `${req.ip},${req.method},${res.statusCode}`,
+        req.originalUrl,
+      );
+    });
     next();
   }
 }
